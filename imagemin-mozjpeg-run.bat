42bc84dd9448fc530a7bfa32f89d4f8a77201e5f
Feng, Cheng-Chi <qwe987299@gmail.com>
2022-10-30 10:50:17 +0800

    V 1.2.1 功能強化與新增說明

--- imagemin-mozjpeg-run.bat
+++ imagemin-mozjpeg-run.bat
@@ -1,6 +1,12 @@
 @echo off
 echo  START!
 START /B /WAIT cmd /c "imagemin-mozjpeg.bat"
+echo  DEL "src"
+rmdir /s/q src\
+mkdir src\
+echo  OPEN "build"
+timeout /t 1
+START build\
 echo  END!
 timeout /t 3
 exit
